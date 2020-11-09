f19c2cb1ec08f4ef92de600f59c6613eef71a3c3
Taylor Boyle <taylorboyle@Taylors-MacBook-Pro-69.local>
2020-11-09 06:03:53 -0600

    adding in inital 'would you like to roll the dice?' prompt/input

--- diceroller.py
+++ diceroller.py
@@ -5,13 +5,18 @@ max = 6
 
 again = True
 
+roll_die = input("Would you like the roll the dice? ").lower()
+if roll_die == "yes" or roll_die == "y":
+    again = True
+else:
+    print("No dice rolled. Have a nice day!")
+    again = False
+
 while again:
     print(random.randint(min, max))
     roll_again = input("Would you like to roll again? ").lower()
     if roll_again == "yes" or roll_again == "y":
         again = True
     else:
-        print("Thanks for using the Dice Roller. Have a nice day.")
+        print("Thanks for using the Dice Roller. Have a nice day!")
         again = False
-
-
